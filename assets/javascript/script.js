c3a059be522d44794d02f5f7d248a9499ac6455a
Sanketcz <sanketzende06@gmail.com>
2023-12-05 12:03:57 +0530

    Added Whole Project to Github Repo

---
+++ assets/javascript/script.js
@@ -0,0 +1,345 @@
+
+
+function getInfofest1(){
+    document.getElementById("infofest1").style.display="block";
+
+    document.getElementById("infofest2").style.display="none";
+    document.getElementById("infofest3").style.display="none";
+    document.getElementById("infofest4").style.display="none";
+}
+function getInfofest2(){
+    document.getElementById("infofest2").style.display="block";
+
+    document.getElementById("infofest1").style.display="none";
+    document.getElementById("infofest3").style.display="none";
+    document.getElementById("infofest4").style.display="none";
+}
+function getInfofest3(){
+    document.getElementById("infofest3").style.display="block";
+
+    document.getElementById("infofest2").style.display="none";
+    document.getElementById("infofest1").style.display="none";
+    document.getElementById("infofest4").style.display="none";
+}
+function getInfofest4(){
+    document.getElementById("infofest4").style.display="block";
+
+    document.getElementById("infofest2").style.display="none";
+    document.getElementById("infofest3").style.display="none";
+    document.getElementById("infofest1").style.display="none";
+}
+//feb...........................
+function getInfofeb1(){
+    document.getElementById("feb1").style.display="block";
+
+    document.getElementById("feb2").style.display="none";
+    document.getElementById("feb3").style.display="none";
+    document.getElementById("feb4").style.display="none";
+}
+function getInfofeb2(){
+    document.getElementById("feb2").style.display="block";
+
+    document.getElementById("feb1").style.display="none";
+    document.getElementById("feb3").style.display="none";
+    document.getElementById("feb4").style.display="none";
+}
+function getInfofeb3(){
+    document.getElementById("feb3").style.display="block";
+
+    document.getElementById("feb1").style.display="none";
+    document.getElementById("feb2").style.display="none";
+    document.getElementById("feb4").style.display="none";
+}
+function getInfofeb4(){
+    document.getElementById("feb4").style.display="block";
+
+    document.getElementById("feb1").style.display="none";
+    document.getElementById("feb2").style.display="none";
+    document.getElementById("feb3").style.display="none";
+}
+//March.................................................
+function getInfomar1(){
+    document.getElementById("mar1").style.display="block";
+
+    document.getElementById("mar2").style.display="none";
+    document.getElementById("mar3").style.display="none";
+    document.getElementById("mar4").style.display="none";
+}
+function getInfomar2(){
+    document.getElementById("mar2").style.display="block";
+
+    document.getElementById("mar1").style.display="none";
+    document.getElementById("mar3").style.display="none";
+    document.getElementById("mar4").style.display="none";
+}
+function getInfomar3(){
+    document.getElementById("mar3").style.display="block";
+
+    document.getElementById("mar1").style.display="none";
+    document.getElementById("mar2").style.display="none";
+    document.getElementById("mar4").style.display="none";
+}
+function getInfomar4(){
+    document.getElementById("mar4").style.display="block";
+
+    document.getElementById("mar1").style.display="none";
+    document.getElementById("mar2").style.display="none";
+    document.getElementById("mar3").style.display="none";
+}
+//April.....................................
+function getInfoapr1(){
+    document.getElementById("apr1").style.display="block";
+
+    document.getElementById("apr2").style.display="none";
+    document.getElementById("apr3").style.display="none";
+}
+function getInfoapr2(){
+    document.getElementById("apr2").style.display="block";
+
+    document.getElementById("apr1").style.display="none";
+    document.getElementById("apr3").style.display="none";
+}
+function getInfoapr3(){
+    document.getElementById("apr3").style.display="block";
+
+    document.getElementById("apr1").style.display="none";
+    document.getElementById("apr2").style.display="none";
+}
+//May.............................
+function getInfomay1(){
+    document.getElementById("may1").style.display="block";
+
+    document.getElementById("may2").style.display="none";
+    document.getElementById("may3").style.display="none";
+    document.getElementById("may4").style.display="none";
+}
+function getInfomay2(){
+    document.getElementById("may2").style.display="block";
+
+    document.getElementById("may1").style.display="none";
+    document.getElementById("may3").style.display="none";
+    document.getElementById("may4").style.display="none";
+}
+function getInfomay3(){
+    document.getElementById("may3").style.display="block";
+
+    document.getElementById("may2").style.display="none";
+    document.getElementById("may1").style.display="none";
+    document.getElementById("may4").style.display="none";
+}
+function getInfomay4(){
+    document.getElementById("may4").style.display="block";
+
+    document.getElementById("may2").style.display="none";
+    document.getElementById("may3").style.display="none";
+    document.getElementById("may1").style.display="none";
+}
+//June.............................
+function getInfojune1(){
+    document.getElementById("june1").style.display="block";
+
+    document.getElementById("june2").style.display="none";
+    document.getElementById("june3").style.display="none";
+    document.getElementById("june4").style.display="none";
+}
+function getInfojune2(){
+    document.getElementById("june2").style.display="block";
+
+    document.getElementById("june1").style.display="none";
+    document.getElementById("june3").style.display="none";
+    document.getElementById("june4").style.display="none";
+}
+function getInfojune3(){
+    document.getElementById("june3").style.display="block";
+
+    document.getElementById("june2").style.display="none";
+    document.getElementById("june1").style.display="none";
+    document.getElementById("june4").style.display="none";
+}
+function getInfojune4(){
+    document.getElementById("june4").style.display="block";
+
+    document.getElementById("june2").style.display="none";
+    document.getElementById("june3").style.display="none";
+    document.getElementById("june1").style.display="none";
+}
+//July.............................................
+function getInfojuly1(){
+    document.getElementById("july1").style.display="block";
+
+    document.getElementById("july2").style.display="none";
+    document.getElementById("july3").style.display="none";
+    document.getElementById("july4").style.display="none";
+}
+function getInfojuly2(){
+    document.getElementById("july2").style.display="block";
+
+    document.getElementById("july1").style.display="none";
+    document.getElementById("july3").style.display="none";
+    document.getElementById("july4").style.display="none";
+}
+function getInfojuly3(){
+    document.getElementById("july3").style.display="block";
+
+    document.getElementById("july2").style.display="none";
+    document.getElementById("july1").style.display="none";
+    document.getElementById("july4").style.display="none";
+}
+function getInfojuly4(){
+    document.getElementById("july4").style.display="block";
+
+    document.getElementById("july2").style.display="none";
+    document.getElementById("july3").style.display="none";
+    document.getElementById("july1").style.display="none";
+}
+//Auguest................................................
+function getInfoaug1(){
+    document.getElementById("aug1").style.display="block";
+
+    document.getElementById("aug2").style.display="none";
+    document.getElementById("aug3").style.display="none";
+    document.getElementById("aug4").style.display="none";
+}
+function getInfoaug2(){
+    document.getElementById("aug2").style.display="block";
+
+    document.getElementById("aug1").style.display="none";
+    document.getElementById("aug3").style.display="none";
+    document.getElementById("aug4").style.display="none";
+}
+function getInfoaug3(){
+    document.getElementById("aug3").style.display="block";
+
+    document.getElementById("aug2").style.display="none";
+    document.getElementById("aug1").style.display="none";
+    document.getElementById("aug4").style.display="none";
+}
+function getInfoaug4(){
+    document.getElementById("aug4").style.display="block";
+
+    document.getElementById("aug2").style.display="none";
+    document.getElementById("aug3").style.display="none";
+    document.getElementById("aug1").style.display="none";
+}
+//Auguest................................................
+function getInfosept1(){
+    document.getElementById("sept1").style.display="block";
+
+    document.getElementById("sept2").style.display="none";
+    document.getElementById("sept3").style.display="none";
+    document.getElementById("sept4").style.display="none";
+}
+function getInfosept2(){
+    document.getElementById("sept2").style.display="block";
+
+    document.getElementById("sept1").style.display="none";
+    document.getElementById("sept3").style.display="none";
+    document.getElementById("sept4").style.display="none";
+}
+function getInfosept3(){
+    document.getElementById("sept3").style.display="block";
+
+    document.getElementById("sept2").style.display="none";
+    document.getElementById("sept1").style.display="none";
+    document.getElementById("sept4").style.display="none";
+}
+function getInfosept4(){
+    document.getElementById("sept4").style.display="block";
+
+    document.getElementById("sept2").style.display="none";
+    document.getElementById("sept3").style.display="none";
+    document.getElementById("sept1").style.display="none";
+}
+//October....................................
+function getInfooct1(){
+    document.getElementById("oct1").style.display="block";
+
+    document.getElementById("oct2").style.display="none";
+    document.getElementById("oct3").style.display="none";
+    document.getElementById("oct4").style.display="none";
+}
+function getInfooct2(){
+    document.getElementById("oct2").style.display="block";
+
+    document.getElementById("oct1").style.display="none";
+    document.getElementById("oct3").style.display="none";
+    document.getElementById("oct4").style.display="none";
+}
+function getInfooct3(){
+    document.getElementById("oct3").style.display="block";
+
+    document.getElementById("oct2").style.display="none";
+    document.getElementById("oct1").style.display="none";
+    document.getElementById("oct4").style.display="none";
+}
+function getInfooct4(){
+    document.getElementById("oct4").style.display="block";
+
+    document.getElementById("oct2").style.display="none";
+    document.getElementById("oct3").style.display="none";
+    document.getElementById("oct1").style.display="none";
+}
+//November......................................
+function getInfonov1(){
+    document.getElementById("nov1").style.display="block";
+
+    document.getElementById("nov2").style.display="none";
+    document.getElementById("nov3").style.display="none";
+    document.getElementById("nov4").style.display="none";
+}
+function getInfonov2(){
+    document.getElementById("nov2").style.display="block";
+
+    document.getElementById("nov1").style.display="none";
+    document.getElementById("nov3").style.display="none";
+    document.getElementById("nov4").style.display="none";
+}
+function getInfonov3(){
+    document.getElementById("nov3").style.display="block";
+
+    document.getElementById("nov2").style.display="none";
+    document.getElementById("nov1").style.display="none";
+    document.getElementById("nov4").style.display="none";
+}
+function getInfonov4(){
+    document.getElementById("nov4").style.display="block";
+
+    document.getElementById("nov2").style.display="none";
+    document.getElementById("nov3").style.display="none";
+    document.getElementById("nov1").style.display="none";
+}
+//December...............................................
+function getInfodec1(){
+    document.getElementById("dec1").style.display="block";
+
+    document.getElementById("dec2").style.display="none";
+    document.getElementById("dec3").style.display="none";
+    document.getElementById("dec4").style.display="none";
+}
+function getInfodec2(){
+    document.getElementById("dec2").style.display="block";
+
+    document.getElementById("dec1").style.display="none";
+    document.getElementById("dec3").style.display="none";
+    document.getElementById("dec4").style.display="none";
+}
+function getInfodec3(){
+    document.getElementById("dec3").style.display="block";
+
+    document.getElementById("dec2").style.display="none";
+    document.getElementById("dec1").style.display="none";
+    document.getElementById("dec4").style.display="none";
+}
+function getInfodec4(){
+    document.getElementById("dec4").style.display="block";
+
+    document.getElementById("dec2").style.display="none";
+    document.getElementById("dec3").style.display="none";
+    document.getElementById("dec1").style.display="none";
+}
+
+/*preloader*/
+var loader=document.getElementById("preloader");
+window.addEventListener("load", function(){
+    loader.style.display="none";
+})
